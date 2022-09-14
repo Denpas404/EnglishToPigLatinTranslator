2b8b64138c20f050043d995a9356f5f4ec3c1da1
Dennis Paaske <depa@pc-tec.local>
2022-09-14 14:24:51 +0200

    First push

---
+++ EnglishToPigLatinTranslatorClassLibrary/Translator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EnglishToPigLatinTranslatorClassLibrary
+{
+    public class Translator
+    {
+        public string TranslateToPigLatinSingleWords(string input)
+        {
+            char[] vowels = new[] { 'A', 'E', 'I', 'O', 'U', 'Å', 'Æ', 'Ø', 'ø', 'æ', 'å', 'a', 'e', 'i', 'o', 'u' };
+            char[] vowelsExtended = vowels.Concat(new[] { 'Y', 'y' }).ToArray();
+            string output = Regex.Replace(input, @"\w+", m =>
+            {
+                string word = m.Value;
+                if (vowels.Contains(word[0])) return word + "yay";
+
+                int indexOfVowel = word.IndexOfAny(vowelsExtended, 1);
+                
+                if (indexOfVowel == -1) return word + "ay";
+                
+                else return word.Substring(indexOfVowel) + word.Substring(0, indexOfVowel) + "ay";
+            });
+            return output;
+        }
+
+        public string TranslateSentence(string word)
+        {
+            List<string> words = word.Split(' ').ToList();
+            string newWord = "";
+
+            foreach (var item in words)
+            {
+                newWord += TranslateToPigLatinSingleWords(item) + " ";
+            }
+
+
+            return newWord.TrimEnd();
+        }
+
+    }
+
+}
---
+++ EnglishToPigLatinTranslatorConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnglishToPigLatinTranslatorClassLibrary;
+
+namespace EnglishToPigLatinTranslatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Translator translator = new Translator();
+            List<string> listOfWords = new List<string>() { "have", "cram", "take", "shrimp", "trebuchet", "ate", "apple", "oaken", "eagle"};
+            List<string> listOfSentences = new List<string>() { "I like to eat honey waffles.", "Do you think it is going to rain today?" };
+
+            foreach (var item in listOfWords)
+            {
+                Console.WriteLine(translator.TranslateToPigLatinSingleWords(item));
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var item in listOfSentences)
+            {
+                Console.WriteLine(translator.TranslateSentence(item));
+            }
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
